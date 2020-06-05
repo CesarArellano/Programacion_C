d371bf9cb4695cbdc3c220e6900bed323b50583f
CesarArellano <raywayday@gmail.com>
2020-06-04 23:21:24 -0500

    Renovando código Serie_2/8.c

--- Serie 2/8.c
+++ Serie 2/8.c
@@ -1,36 +1,56 @@
+/*
+*@file 8.c
+*@brief Escriba un algoritmo que permita convertir una frase introducida en mayúsculas y minúsculas
+*a una frase de sólo mayúsculas y a una frase de sólo minúsculas.
+*@author César Arellano
+*@date 04/06/2020
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 
-void LeerFrase(char Frase[]);
-void Convertir(char Frase[], char Mayus[], char Minus[]); 
+void convertirCadenas(char fraseOriginal[],char fraseMay[], char fraseMin[]);
 
-int main(void)
+int main (void)
 {
-	char Frase[200],Mayus[200],Minus[200];
-	LeerFrase(Frase);
-	Convertir(Frase,Mayus,Minus);
-	puts(Mayus);
-	puts(Minus);
-	return 0;
+  char frase[200],fraseMayusculas[200],fraseMinusculas[200];
+  printf("Escribe la frase: ");
+  scanf(" %[^\n]",frase);
+  convertirCadenas(frase,fraseMayusculas,fraseMinusculas);
+  system("clear"); // Borrar contenido de la terminal.
+  printf("Frase original: %s\n",frase);
+  printf("Frase en mayúsculas: %s\n",fraseMayusculas);
+  printf("Frase en minúsculas: %s\n",fraseMinusculas);
+  return 0;
 }
-void LeerFrase(char Frase[])
-{
-	printf("Escriba la frase\n");
-	scanf(" %[^\n]",Frase);
-}
-void Convertir(char Frase[], char Mayus[], char Minus[])
+
+/*
+*@brief Esta función convierte la frase original en mayúsculas y en minúsculas (La devuelve en una cadena distinta).
+*@author César Arellano
+*@param char fraseOriginal[]
+*@param char fraseMay[]
+*@param char fraseMin[]
+*@return void
+*/
+void convertirCadenas(char fraseOriginal[],char fraseMay[], char fraseMin[])
 {
 	int i;
-	for(i=0;Frase[i]!='\0';i++)
+
+	for(i = 0; fraseOriginal[i] != '\0'; i++)
 	{
-		if(Frase[i]>='a' && Frase[i]<='z')
-			Mayus[i]=Frase[i]-'a'+'A';
-		else
-			Mayus[i]=Frase[i];
-		if(Frase[i]>='A' && Frase[i]<='Z')
-			Minus[i]=Frase[i]-'A'+'a';
-		else
-			Minus[i]=Frase[i];
+		fraseMay[i] = fraseOriginal[i];
+	    fraseMin[i] = fraseOriginal[i];
+
+	    if(fraseOriginal[i] >= 'a' && fraseOriginal[i] <='z') // Convertir a mayúsculas
+	    {
+	      fraseMay[i] = fraseOriginal[i]-'a'+'A';
+	    }
+	    if(fraseOriginal[i] >= 'A' && fraseOriginal[i] <='Z') // Convertir a minúsculas
+	    {
+	      fraseMin[i] = fraseOriginal[i]-'A'+'a';
+	    }
 	}
-	Mayus[i]='\0';
-	Minus[i]='\0';
+
+	fraseMay[i] = '\0'; // Colocamos caracter de fin de línea a las dos cadenas.
+	fraseMin[i] = '\0';
 }
